8b1887a77bbecd99800d5982141c43389d051ae5
riyu.iring <oribelloruthiemy@gmail.com>
2025-09-05 02:39:39 +0800

    Add: Add PageSync.jsx

---
+++ frontend/src/components/PageSync.jsx
@@ -0,0 +1,12 @@
+import usePageStore from "@/store/usePageStore"
+import { useEffect } from "react";
+
+export function PageSync({page}){
+    const { setCurrentPage } = usePageStore();
+
+    useEffect(() => {
+        setCurrentPage(page)
+    }, [setCurrentPage , page]) 
+
+    return null
+}
