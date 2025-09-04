60ea543394b063cd13e8b68192555552d98b67dd
riyu.iring <oribelloruthiemy@gmail.com>
2025-09-05 02:46:11 +0800

    Update: Update user data sample

--- frontend/src/test/user_data.js
+++ frontend/src/test/user_data.js
@@ -1,21 +1,31 @@
 
-export const userData = [
+export const data = [
     {
         id: 1,
         name: "John Doe",
         email: "john.doe@example.com",
-        password: "password"
+        password: "password",
+        role: "user"
     },
     {
         id: 2,
         name: "Jane Smith",
         email: "jane.smith@example.com",
-        password: "password"
+        password: "password",
+        role: "user"
     },
     {
         id: 3,
         name: "Alice Johnson",
         email: "alice.johnson@example.com",
-        password: "password"
+        password: "password",
+        role: "user"
+    },
+    {
+        id: 1,
+        name: "Admin #1",
+        email: "admin@example.com",
+        password: "password",
+        role: "admin"
     }
 ]
