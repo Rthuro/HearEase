a8cd74c549b21ca62c0e9a47f5bb41416d3ee5fa
riyu.iring <oribelloruthiemy@gmail.com>
2025-09-09 01:57:08 +0800

    Add: add custom components

---
+++ frontend/src/components/FileCase.jsx
@@ -0,0 +1,45 @@
+import file_case_img from '@/assets/imgs/appointment_form.png'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import { ChevronRight } from 'lucide-react'
+import { nanoid } from 'nanoid'
+import { dateFormatter } from '@/lib/helpers'
+import { useNavigate } from 'react-router-dom'
+import useAuthenticationStore from '@/store/useAuthenticationStore'
+
+export function FileCase(){
+    const { userRole, userInfo } = useAuthenticationStore();
+
+    const today = dateFormatter(new Date());
+    const generateCaseNumber = () => {
+        return nanoid(11);
+    }
+    const navigate = useNavigate();
+
+    return (
+        <main className='flex flex-col w-full items-center justify-center gap-6 mt-14 '>
+            <img src={file_case_img} alt="File Case" className='w-[150px] object-cover' />
+            <h1 className='text-3xl text-redBase text-center'>File New Case</h1>
+            <div className="flex items-center gap-6">
+                <div className="flex items-center relative">
+                    <p className='absolute top-0 left-2 bottom-0 self-center text-zinc-500 '>Case Number:</p>
+                    <Input type="text" className='bg-white w-72 text-end pl-4' 
+                    value={generateCaseNumber()} readOnly/>
+                </div>
+                <div className="flex items-center relative">
+                    <p className='absolute top-0 left-2 bottom-0 self-center text-zinc-500 '>Date Filling:</p>
+                    <Input type="text" className='bg-white w-72 text-end pl-4'
+                    value={today}  readOnly
+                /> 
+                </div>
+
+            </div>
+            <Button onClick={userRole === 'admin' ? () => navigate('/Admin/File-Case/Case-Form') : () => navigate('/@' + userInfo.name.replace(" ", "_") + 
+                '/File-Case/Case-Form')} 
+                className='!bg-redBase hover:bg-red-700 text-white flex items-center justify-between font-normal cursor-pointer '>
+                Start creating
+                <ChevronRight />
+            </Button>
+        </main>
+    )
+}
