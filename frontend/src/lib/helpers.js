d751b95652afb790ff079678b8c01e12b703f54c
riyu.iring <oribelloruthiemy@gmail.com>
2025-09-09 20:09:30 +0800

    Update: add get first hearing date function in helpers.jsx

--- frontend/src/lib/helpers.js
+++ frontend/src/lib/helpers.js
@@ -9,3 +9,9 @@ export const getBarangayNames = () => barangays.map(b => b.name);
 export const getStreets = (barangayName) => {
     return barangays.find(b => b.name === barangayName)?.streets || [];
 }
+
+export function getFirstHearingDate(scheduledDate = new Date()) {
+  const date = new Date(scheduledDate);
+  date.setDate(date.getDate() + 7); 
+  return date;
+}
