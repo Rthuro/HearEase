2e4a3b7108632ce939e88eda1196723c8caa84a8
riyu.iring <oribelloruthiemy@gmail.com>
2025-09-13 01:47:03 +0800

    Add: added Hearings page

---
+++ frontend/src/routes/user/Hearings.jsx
@@ -0,0 +1,55 @@
+import { UserHearings } from "@/components/user_ui/UserHearings";
+import { Input } from "@/components/ui/input";
+import { Search } from 'lucide-react'
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useState } from "react";
+import { ChevronDown } from "lucide-react";
+import { PageSync } from "@/components/PageSync";
+
+
+export function Hearings() {
+    const [status, setStatus] = useState("all");
+
+    return (
+        <div className="p-6 flex flex-col gap-2">
+            <PageSync page="Hearings" />
+            <h1 className="text-2xl font-bold">Hearings</h1>
+            <p>You have <span className="font-medium text-redBase">0</span> upcoming hearings.</p>
+            
+            
+            <section className="flex flex-col gap-3 mt-3 bg-white border border-zinc-200 rounded-lg p-6">
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                        <Search className="text-zinc-400 ml-3" size={16} />
+                        <Input type="text" placeholder="Search for case..." className="w-72 -ml-6 pl-8" />
+                    </div>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline">
+                                Sort by
+                                <ChevronDown className="ml-2 h-4 w-4" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-38">
+                            <DropdownMenuRadioGroup value={status} onValueChange={setStatus}>
+                            <DropdownMenuRadioItem value="all">All</DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="upcoming">Upcoming</DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="past">Past</DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="escalated">Escalated</DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value="cancelled">Cancelled</DropdownMenuRadioItem>
+                            </DropdownMenuRadioGroup>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+                <UserHearings hearings={[]} showPagination={true} />
+            </section>
+        </div>
+    );
+}
