abc4f165be2e23e7bd1811cac29769608201bef4
riyu.iring <oribelloruthiemy@gmail.com>
2025-09-28 18:30:40 +0800

    Add: recent case records for admin dashboard

---
+++ frontend/src/components/RecentCaseRecords.jsx
@@ -0,0 +1,31 @@
+// For dashboard use to show preview case records
+import { Link } from "react-router-dom";
+import { CaseStatusDisplay } from "./CaseStatusDisplay";
+import folder_img from '@/assets/folder.png'
+import { FolderOpen } from "lucide-react"
+import { getNatureLabel } from "@/lib/helpers";
+
+export function RecentCaseRecords({cases, user}) {
+
+    return (
+        <div className="flex items-center flex-wrap gap-3 ">
+            {cases?.length > 0 ? (
+                    cases?.map((caseItem) => (
+                        <Link to={`/${user}/Case/${caseItem.case_number}`} key={caseItem.case_number} className="border border-zinc-200 rounded-lg p-4 w-60 hover:shadow-md transition-shadow">
+                            <CaseStatusDisplay caseStatus={caseItem.case_status} />
+                            <img src={folder_img} alt="folder" className="mx-auto mb-2"/>
+                            <p className="font-medium text-sm mb-1 text-center">{caseItem.case_number}</p>
+                            <p className="text-sm text-zinc-600 mb-1 text-center">{getNatureLabel(caseItem?.nature_of_complaint_code)}
+                            </p>
+                        </Link>
+                    )
+                )
+                ) : (
+                    <div className="flex flex-col gap-2 items-center mx-auto text-zinc-600 my-6">
+                        <FolderOpen />
+                        <p>No cases made.</p>
+                    </div>
+            )}
+        </div>
+    )
+}
