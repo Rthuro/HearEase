853400d50e0694f24b08c6695af87aabb8940551
riyu.iring <oribelloruthiemy@gmail.com>
2025-09-30 03:34:02 +0800

    Add: add lupon management and search case page

---
+++ frontend/src/routes/admin/pages/LuponManagement.jsx
@@ -0,0 +1,8 @@
+
+export function LuponManagement(){
+    return(
+        <div>
+            Lupon Management
+        </div>
+    )
+}
---
+++ frontend/src/routes/admin/pages/SearchCase.jsx
@@ -0,0 +1,8 @@
+
+export function SearchCase(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
