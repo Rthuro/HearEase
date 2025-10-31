53eefe696fa8cc8a178ecc9cce26798c36470d34
riyu.iring <oribelloruthiemy@gmail.com>
2025-11-01 00:46:06 +0800

    Update: fix case link

--- frontend/src/components/RecentCaseRecords.jsx
+++ frontend/src/components/RecentCaseRecords.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { CaseStatusDisplay } from "./CaseStatusDisplay";
 import folder_img from '@/assets/folder.png'
 import { FolderOpen } from "lucide-react"
-import { getNatureLabel } from "@/lib/helpers";
 
 export function RecentCaseRecords({cases, user}) {
 
@@ -11,11 +10,11 @@ export function RecentCaseRecords({cases, user}) {
         <div className="flex items-center flex-wrap gap-3 ">
             {cases?.length > 0 ? (
                     cases?.map((caseItem) => (
-                        <Link to={`/${user}/Case/${caseItem.case_number}`} key={caseItem.case_number} className="border border-zinc-200 rounded-lg p-4 w-60 hover:shadow-md transition-shadow">
+                        <Link to={`/${user}/Case/${caseItem.id}`} key={caseItem.id} className="border border-zinc-200 rounded-lg p-4 w-60 hover:shadow-md transition-shadow">
                             <CaseStatusDisplay caseStatus={caseItem.case_status} />
                             <img src={folder_img} alt="folder" className="mx-auto mb-2"/>
-                            <p className="font-medium text-sm mb-1 text-center">{caseItem.case_number}</p>
-                            <p className="text-sm text-zinc-600 mb-1 text-center">{getNatureLabel(caseItem?.nature_of_complaint_code)}
+                            <p className="font-medium text-sm mb-1 text-center">{caseItem.id}</p>
+                            <p className="text-sm text-zinc-600 mb-1 text-center">{caseItem.case_type.case_name}
                             </p>
                         </Link>
                     )
