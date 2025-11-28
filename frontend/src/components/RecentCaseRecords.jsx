6e205a84be91de45faa04f84ee86a936e3ab5591
riyu.iring <oribelloruthiemy@gmail.com>
2025-11-29 00:05:54 +0800

    Update: update component

--- frontend/src/components/RecentCaseRecords.jsx
+++ frontend/src/components/RecentCaseRecords.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 import { CaseStatusDisplay } from "./CaseStatusDisplay";
 import folder_img from '@/assets/folder.png'
 import { FolderOpen, Ellipsis, Check, X } from "lucide-react"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useCaseStore } from "@/store/useCaseStore";
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuTrigger,
+// } from "@/components/ui/dropdown-menu"
+// import { useCaseStore } from "@/store/useCaseStore";
 
 export function RecentCaseRecords({cases, user}) {
-    const { deleteCase } = useCaseStore();
+    // const { deleteCase } = useCaseStore();
 
     return (
         <div className="flex items-center flex-wrap gap-3 ">
@@ -21,14 +21,14 @@ export function RecentCaseRecords({cases, user}) {
                         <Link to={`/${user}/Case/${caseItem?.id}`} key={caseItem?.id} className="border border-zinc-200 rounded-lg p-4 w-60 hover:shadow-md transition-shadow">
                             <div className="flex justify-between">
                                 <CaseStatusDisplay caseStatus={caseItem?.case_status} />
-                                <DropdownMenu>
+                                {/* <DropdownMenu>
                                     <DropdownMenuTrigger asChild  >
                                         <Ellipsis />
                                     </DropdownMenuTrigger>
                                     <DropdownMenuContent className="w-fit" align="end">
-                                            {caseItem?.case_status != 'filed' ? (
+                                            {caseItem?.case_status != 'approved' ? (
                                                 <div>
-                                                    <DropdownMenuItem value="archive">Archive</DropdownMenuItem>
+                                                    <DropdownMenuItem value="archive">Draft</DropdownMenuItem>
                                                     {caseItem?.case_status === 'pending_approval' && (
                                                         <DropdownMenuItem value="delete" onClick={() => deleteCase(caseItem?.id)}>Delete</DropdownMenuItem>
                                                     )}
@@ -46,7 +46,7 @@ export function RecentCaseRecords({cases, user}) {
                                                 </div>
                                             )}
                                     </DropdownMenuContent>
-                                </DropdownMenu>
+                                </DropdownMenu> */}
                             </div>
                             <img src={folder_img} alt="folder" className="mx-auto mb-2"/>
                             <p className="font-medium text-sm mb-1 text-center">{caseItem?.id}</p>
