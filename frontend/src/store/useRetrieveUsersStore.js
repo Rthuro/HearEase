3c9744e141aea4bc0dcbca88fc8ab7a64177cecd
riyu.iring <oribelloruthiemy@gmail.com>
2025-11-29 00:02:28 +0800

    Add: add fetch function for co complainants and respondents

--- frontend/src/store/useRetrieveUsersStore.js
+++ frontend/src/store/useRetrieveUsersStore.js
@@ -20,6 +20,26 @@ export const getUsers = async () => {
     return response.data.users;
 }
 
+export const getUser = async (email) => {
+    const response = await axios.post(`${API_URL}/find-user`, { 
+        email: email
+     });
+    return response.data.user;
+}
+
+export const getCaseCoRespondents = async (corespondent_ids) => {
+    const response = await axios.post(`${API_URL}/get-case-co-respondents/`, {
+        ids: corespondent_ids
+    });
+    return response.data;
+}
+export const getCaseCoComplainants = async (cocomplainant_ids) => {
+    const response = await axios.post(`${API_URL}/get-case-co-complainants/`, {
+        ids: cocomplainant_ids
+    });
+    return response.data;
+}
+
 export const useRetrieveUsersStore = create( (set) => ({
     complainantsUsers: [],    
     fetchComplainants: async () => {
@@ -44,6 +64,24 @@ export const useRetrieveUsersStore = create( (set) => ({
         try {
             const data = await getRespondents();
             set({ respondents: data })
+        } catch (error) {
+            toast.error(error);
+        }
+    },
+    caseCoComplainants:[],
+    fetchCaseCoComplainants: async (Ids) => {
+        try {
+            const data = await getCaseCoComplainants(Ids);
+            set({ caseCoComplainants: data });
+        } catch (error) {
+            toast.error(error);
+        }
+    },
+    caseCoRespondents:[],
+    fetchCaseCoRespondents: async (Ids) => {
+        try {
+            const data = await getCaseCoRespondents(Ids);
+            set({ caseCoRespondents: data });
         } catch (error) {
             toast.error(error);
         }
