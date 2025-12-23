d231939897b608c2fe5d704e7ca2564792820b32
riyu.iring <oribelloruthiemy@gmail.com>
2025-12-23 23:43:32 +0800

    Update: uodate build.sh file

--- backend/build.sh
+++ backend/build.sh
@@ -7,5 +7,19 @@ pip install -r requirements.txt
 # Convert static files
 python manage.py collectstatic --no-input
 
-# Apply migrations (Supabase)
 python manage.py migrate
+
+# --- SEEDING DATA ---
+python manage.py seed_barangays
+python manage.py seed_case_types
+python manage.py seed_relationships
+python manage.py seed_settlements
+
+# --- CREATING DOCUMENTS ---
+python manage.py create_appointment_form
+python manage.py create_cancellation_notice
+python manage.py create_court_certification
+python manage.py create_default_templates
+python manage.py create_monitoring_sheet
+python manage.py create_no_show_notice
+python manage.py create_summon_letter
